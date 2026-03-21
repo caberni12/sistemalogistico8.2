4add5279d339f2d83d5e776d2c3f0ff87cedc969
caberni12 <alejandrosilva1891@gmail.com>
2026-03-21 07:52:13 -0300

    Update envio2.js

--- envio2.js
+++ envio2.js
@@ -2,7 +2,7 @@
 //const API="https://script.google.com/macros/s/AKfycbzj3sRVqYDgGVak1PNHrycYQ6FI5Mk5UyADOL0uI4CDAprlT7LDv3ZVWrfMCkwPMCgW/exec";
 
 
-const API="https://script.google.com/macros/s/AKfycbzodC_hETw5gmDkEHejyY3N3i1PAiXunxr0_utjw6u98YdXObYahjsScuGMIziQ8uWg/exec";
+const API="https://script.google.com/macros/s/AKfycbwedc44Y_OpDr8XM0cm3IMMsnRtk0XgvSoPxSuya_uSFgc1QuxDa3xSFBmDAD5aL-gK/exec";
 //https://script.google.com/macros/s/AKfycbzj3sRVqYDgGVak1PNHrycYQ6FI5Mk5UyADOL0uI4CDAprlT7LDv3ZVWrfMCkwPMCgW/exec
 
 let RAW=[];
